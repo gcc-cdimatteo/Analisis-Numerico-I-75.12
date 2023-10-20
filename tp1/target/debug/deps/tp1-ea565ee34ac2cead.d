72c362d4fd3371b6574470bdbe30c3786ae9fdf4
Fran Orquera <franorquera@Frans-MacBook-Pro.local>
2023-10-19 23:21:43 -0300

    arreglo problema en la derivada de f(x)

---
+++ tp1/target/debug/deps/tp1-ea565ee34ac2cead.d
@@ -0,0 +1,5 @@
+/Users/franorquera/Documents/FIUBA/Numerico/Analisis-Numerico-I-75.12/tp1/target/debug/deps/tp1-ea565ee34ac2cead.rmeta: src/main.rs
+
+/Users/franorquera/Documents/FIUBA/Numerico/Analisis-Numerico-I-75.12/tp1/target/debug/deps/tp1-ea565ee34ac2cead.d: src/main.rs
+
+src/main.rs:
---
+++ tp1/target/debug/deps/tp1-efb3380688b6e50d.d
@@ -0,0 +1,5 @@
+/Users/franorquera/Documents/FIUBA/Numerico/Analisis-Numerico-I-75.12/tp1/target/debug/deps/tp1-efb3380688b6e50d.rmeta: src/main.rs
+
+/Users/franorquera/Documents/FIUBA/Numerico/Analisis-Numerico-I-75.12/tp1/target/debug/deps/tp1-efb3380688b6e50d.d: src/main.rs
+
+src/main.rs:
